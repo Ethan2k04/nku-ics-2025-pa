645967862be6e1ab51214501b1db9138026d22d4
tracer-ics2017 <tracer@njuics.org>
2025-03-09 13:01:46 +0800

    >  compile 2212202 ubuntu Linux ubuntu-virtual-machine 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  13:01:46 up  5:00,  1 user,  load average: 0.10, 0.05, 0.08 ee36d76941e9bb09c117a0bdfa945b848571323f

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -39,7 +39,7 @@ static int cmd_q(char *args) {
 static int cmd_help(char *args);
 
 static int cmd_si(char *args) {
-  if(args[0] == NULL) {
+  if(args == NULL) {
     cpu_exec(1);
     return 0;
   }
