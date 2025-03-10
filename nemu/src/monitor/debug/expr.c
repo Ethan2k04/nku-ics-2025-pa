686616c9e695d40fcaa3231297a994833748ce3e
Ethan2k04 <3287816638@qq.com>
2025-03-10 13:11:23 +0800

    PA1 part2 parser part half finished, need to impl the parse tree and handle dereference opration.

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -10,7 +10,7 @@ enum {
   TK_NOTYPE = 256, TK_EQ,
 
   /* TODO: Add more token types */
-  TK_NEQ, TK_NUM, TK_HEX, TK_REG, TK_SYM, TK_NG, TK_NL, TK_AND, TK_OR, TK_DEREF, TK_NEG
+  TK_NEQ, TK_NUM, TK_HEX, TK_REG, TK_SYMB, TK_NG, TK_NL, TK_AND, TK_OR, TK_DEREF, TK_NEG
 };
 
 static struct rule {
@@ -35,21 +35,19 @@ static struct rule {
   {"\\$e[a,b,c,d]x", TK_REG},         // register (eax, ebx, ecx, edx)
   {"\\$e[s,b]p", TK_REG},             // register (esp, ebp)
   {"\\$e[d,s]i", TK_REG},             // register (edi, esi)
-  {"[a-zA-Z_][a-zA-Z0-9_]*", TK_SYM}, // symbol
+  {"[a-zA-Z_][a-zA-Z0-9_]*", TK_SYMB}, // symbol
   {"<=", TK_NG},                      // not greater than
   {">=", TK_NL},                      // not less than
   {"<", '<'},                         // less than
   {">", '>'},                         // greater than
-  {"&&", TK_AND},                     // and
-  {"\\|\\|", TK_OR},                  // or
-  {"!", '!'},                         // not
-  {"\\^", '^'},                       // xor
-  {"~", '~'},                         // neg
   {"\\(", '('},                       // left bracket
   {"\\)", ')'},                       // right bracket
   {"&&", TK_AND},                     // and
   {"\\|\\|", TK_OR},                  // or
   {"&", '&'},                         // get addr
+  {"\\^", '^'},                       // xor
+  {"!", '!'},                         // not
+  {"~", '~'},                         // neg
 };
 
 #define NR_REGEX (sizeof(rules) / sizeof(rules[0]) )
@@ -104,21 +102,21 @@ static bool make_token(char *e) {
          * of tokens, some extra actions should be performed.
          */
 
-	int j;
+	      int j;
         switch (rules[i].token_type) {
           case TK_NOTYPE:
             break;
           case TK_REG:
           case TK_HEX:
           case TK_NUM:
-          case TK_SYM:
+          case TK_SYMB:
             for(j = 0; j < substr_len; j ++) { tokens[nr_token].str[j] = *(substr_start + j); }
 	          tokens[nr_token].str[j]='\0';
-            Log("Token %d: Copied substring \"%s\" to tokens[%d].str (type: %d)",
-                nr_token, tokens[nr_token].str, nr_token, rules[i].token_type);
+            // Log("Token %d: Copied substring \"%s\" to tokens[%d].str (type: %d)",
+            //     nr_token, tokens[nr_token].str, nr_token, rules[i].token_type);
           default:
             tokens[nr_token].type = rules[i].token_type;
-            Log("Token %d: Set type to %d (no string copied)", nr_token, rules[i].token_type);
+            // Log("Token %d: Set type to %d (no string copied)", nr_token, rules[i].token_type);
             nr_token ++;
         }
 
@@ -133,6 +131,84 @@ static bool make_token(char *e) {
   }
 
   return true;
+}
+
+int check_parentheses(int p, int q) {
+  int i, unmatched = 0;
+  /* Iterate through the tokens array to check whether the number of left/right parentheses are matched. */
+  for (i = p; i <= q; i++) {
+    if (tokens[i].type == '(') {  ++unmatched; }
+    else if (tokens[i].type == ')') {  --unmatched; }
+  }
+  return (unmatched == 0) && (tokens[p].type == '(') && (tokens[q].type == ')');
+}
+
+uint32_t eval(int p, int q, bool *success) {
+  if (p > q) {
+    /* Bad expression */
+    *success = false;
+    return 0;
+  }
+  else if (p == q) {
+    /* Single token. 
+     * For now this token should be a number.
+     * Return the value of the number.
+     */
+    if (tokens[p].type == TK_NUM) {
+      uint32_t parsed_value = 0;
+      sscanf(tokens[p].str, "%d", &parsed_value);
+      return parsed_value;
+    }
+    else if (tokens[p].type == TK_HEX) {
+      uint32_t parsed_value = 0;
+      sscanf(tokens[p].str, "%x", &parsed_value);
+      return parsed_value;
+    }
+    else if (token[p].type == TK_REG) {
+      if (strcmp(tokens[p].str, "$eax") == 0) {
+        return cpu.eax;
+      }
+      else if (strcmp(tokens[p].str, "$ebx") == 0) {
+        return cpu.ebx;
+      }
+      else if (strcmp(tokens[p].str, "$ecx") == 0) {
+        return cpu.ecx;
+      }
+      else if (strcmp(tokens[p].str, "$edx") == 0) {
+        return cpu.edx;
+      }
+      else if (strcmp(tokens[p].str, "$esp") == 0) {
+        return cpu.esp;
+      }
+      else if (strcmp(tokens[p].str, "$ebp") == 0) {
+        return cpu.ebp;
+      }
+      else if (strcmp(tokens[p].str, "$esi") == 0) {
+        return cpu.esi;
+      }
+      else if (strcmp(tokens[p].str, "$edi") == 0) {
+        return cpu.edi;
+      }
+      else {
+        return cpu.eip;
+      }
+    }
+    else {
+      /* TODO: Handle TK_SYMB type */
+      TODO();
+    }
+    return 0;
+  }
+  else if (check_parentheses(p, q) == true) {
+    /* The expression is surrounded by a matched pair of parentheses.
+     * If that is the case, just throw away the parentheses.
+     */
+    return eval(p + 1, q - 1, success);
+  }
+  else {
+    /* We should do more things here. */
+    TODO();
+  }
 }
 
 uint32_t expr(char *e, bool *success) {
