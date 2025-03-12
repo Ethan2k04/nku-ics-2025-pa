dc6ab478c13df56934b46db8d3a8b791a2b658a8
tracer-ics2017 <tracer@njuics.org>
2025-03-12 10:47:53 +0800

    >  compile 2212202 ubuntu Linux ubuntu-virtual-machine 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  10:47:53 up 11:18,  1 user,  load average: 0.07, 0.07, 0.02 678015b266f0fb15688aa84de45eef987f506c7

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -117,6 +117,7 @@ static bool make_token(char *e) {
           for(j = 0; j < substr_len; ++j) { tokens[nr_token].str[j] = *(substr_start + j); }
 	        tokens[nr_token].str[j]='\0';
         }
+	break;
       }
     }
 
