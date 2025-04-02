93c41afa4b8c8e06b95ce931394fb7e39fd1ac6f
2212202-Yin Tengjiao <2212202@mail.nankai.edu.cn>
2025-04-02 20:42:45 +0800

    add decl support for existing EHelper functions in all-instr.h

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -1,23 +1,45 @@
 #include "cpu/exec.h"
 
-make_EHelper(mov);
-
-make_EHelper(operand_size);
-
-make_EHelper(inv);
-make_EHelper(nemu_trap);
-
+/* arith.c */
 make_EHelper(add);
 make_EHelper(sub);
-make_EHelper(xor);
 make_EHelper(inc);
 make_EHelper(dec);
+make_EHelper(adc);
+make_EHelper(sbb);
+make_EHelper(mul);
+make_EHelper(imul1);
+make_EHelper(imul2);
+make_EHelper(imul3);
+make_EHelper(div);
+make_EHelper(idiv);
 
+/* control.c */
+make_EHelper(jmp);
+make_EHelper(jcc);
+make_EHelper(jmp_rm);
+make_EHelper(call);
+make_EHelper(ret);
+
+/* data-mov.c */
+make_EHelper(mov);
 make_EHelper(push);
 make_EHelper(pop);
 make_EHelper(leave);
 make_EHelper(cwtl);
 make_EHelper(cltd);
+make_EHelper(movsx);
+make_EHelper(movzx);
+make_EHelper(lea);
 
-make_EHelper(call);
-make_EHelper(ret);
+/* logic.c */
+make_EHelper(xor);
+make_EHelper(setcc);
+
+/* prefix.c */
+make_EHelper(operand_size);
+
+/* special.c */
+make_EHelper(nop);
+make_EHelper(inv);
+make_EHelper(nemu_trap);
