7ec788c8a283a7f254a7dfa9dfc12878bf763a01
tracer-ics2017 <tracer@njuics.org>
2025-04-02 10:04:06 +0800

    >  compile 2212202 ubuntu Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  10:04:06 up  1:03,  1 user,  load average: 0.61, 0.62, 0.55 e57cb97d4ab59de5ab52b0ce4c8362d91c75a1b

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -195,7 +195,8 @@ static inline void rtl_update_ZF(const rtlreg_t* result, int width) {
 
 static inline void rtl_update_SF(const rtlreg_t* result, int width) {
   // eflags.SF <- is_sign(result[width * 8 - 1 .. 0])
-  TODO();
+  // TODO();
+  cpu.eflags.SF = (*result >> (width * 8 - 1)) & 0x1;
 }
 
 static inline void rtl_update_ZFSF(const rtlreg_t* result, int width) {
