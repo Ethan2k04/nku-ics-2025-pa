ca51b3eb82637a3cda0dacc7f480f47a4b42cc45
tracer-ics2017 <tracer@njuics.org>
2025-04-23 09:30:42 +0800

    >  compile 2212202 ubuntu Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  09:30:42 up 19 min,  1 user,  load average: 0.74, 0.58, 0.46 166459004f4c4ebc674272891950d8b1fa5f772

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -2,6 +2,9 @@
 
 #define DEFAULT_ENTRY ((void *)0x4000000)
 
+extern void ramdisk_read(void *buf, off_t offset, size_t len);
+extern size_t get_ramdisk_size();
+
 uintptr_t loader(_Protect *as, const char *filename) {
   // TODO();
   size_t size = get_ramdisk_size();
