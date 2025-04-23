a13a6792a4e9fe1e93ab2409d4095078b05b820c
2212202-Yin Tengjiao <2212202@mail.nankai.edu.cn>
2025-04-23 09:28:09 +0800

    implement a simple loader in loader.c

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -3,6 +3,10 @@
 #define DEFAULT_ENTRY ((void *)0x4000000)
 
 uintptr_t loader(_Protect *as, const char *filename) {
-  TODO();
+  // TODO();
+  size_t size = get_ramdisk_size();
+  void *buf = NULL;
+  ramdisk_read(buf, 0, size);
+  memcpy(DEFAULT_ENTRY, buf, size);
   return (uintptr_t)DEFAULT_ENTRY;
 }
