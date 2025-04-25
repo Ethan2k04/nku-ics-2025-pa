04fc65380b0e11455651e3d9fa36df46d4c42169
tracer-ics2017 <tracer@njuics.org>
2025-04-25 19:10:14 +0800

    >  run 2212202 ubuntu Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  19:10:14 up  1:05,  1 user,  load average: 1.35, 1.08, 0.93 3450e4dfb12aae5565f458aebc310837d993aa0

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -4,12 +4,28 @@
 
 extern void ramdisk_read(void *buf, off_t offset, size_t len);
 extern size_t get_ramdisk_size();
+extern int fs_open(const char *pathname, int flags, mode_t mode);
+extern ssize_t fs_read(int fd, void *buf, size_t count);
+extern int fs_close(int fd);
+extern size_t fs_sizez(int fd);
 
 uintptr_t loader(_Protect *as, const char *filename) {
   // TODO();
-  size_t size = get_ramdisk_size();
+  // size_t size = get_ramdisk_size();
+  // void *buf = NULL;
+  // ramdisk_read(buf, 0, size);
+  // memcpy(DEFAULT_ENTRY, buf, size);
+  // return (uintptr_t)DEFAULT_ENTRY;
+  int fd = fs_open(filename, 0, 0);
+  int fs_size = fs_sizez(fd);
   void *buf = NULL;
-  ramdisk_read(buf, 0, size);
-  memcpy(DEFAULT_ENTRY, buf, size);
+
+  Log("Loading file %s [fd: %d] with size %d bytes", filename, fd, fs_size);
+
+  if (fs_size > 0) {
+    fs_read(fd, buf, fs_size);
+    memcpy(DEFAULT_ENTRY, buf, fs_size);
+  }
+
   return (uintptr_t)DEFAULT_ENTRY;
 }
