c72a3278df6a4b2d64dea949635970f57fce146f
tracer-ics2017 <tracer@njuics.org>
2025-05-14 14:03:19 +0800

    >  run 2212202 ubuntu Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  14:03:19 up  1:56,  1 user,  load average: 0.27, 0.43, 0.53 119a57a22750f916788dc264e49b7866ed27e57b

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -5,6 +5,7 @@ extern _RegSet* do_syscall(_RegSet *r);
 static _RegSet* do_event(_Event e, _RegSet* r) {
   switch (e.event) {
     case _EVENT_SYSCALL: return do_syscall(r);
+    case _EVENT_TRAP: { printf("Nanos-lite received _EVENT_TRAP but _umake() unimplemented."); return NULL;}
     default: panic("Unhandled event ID = %d", e.event);
   }
 
