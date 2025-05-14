9abfdf30d187f413e86b57282b7d27756191291f
tracer-ics2017 <tracer@njuics.org>
2025-05-14 09:34:00 +0800

    >  compile 2212202 ubuntu Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  09:34:00 up 59 min,  1 user,  load average: 1.06, 0.84, 0.67 f5adeafe99956c8761847a27a1d4f3448afd792

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -55,6 +55,7 @@ paddr_t page_translate(vaddr_t addr, bool iswrite) {
 }
 
 uint32_t vaddr_read(vaddr_t addr, int len) {
+  printf("FUCK I AM OKAY!");
   if ((addr & PAGE_MASK) != ((addr + len - 1) & PAGE_MASK)) {
     // Concatenate the data if it cross the page boundary
     uint32_t data = 0x0;
