167f6281e666c5d1d927fbe83ae849ab98f15f6c
tracer-ics2017 <tracer@njuics.org>
2025-06-01 10:26:05 +0800

    >  run 2212202 ubuntu Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  10:26:05 up  6:57,  1 user,  load average: 0.45, 0.62, 0.88 40863fce4544d0bf26a9efbea869f2fbf0e9c72

--- navy-apps/apps/pal/src/FLOAT/FLOAT.c
+++ navy-apps/apps/pal/src/FLOAT/FLOAT.c
@@ -2,6 +2,15 @@
 #include <stdint.h>
 #include <assert.h>
 
+union float_f {
+  struct {
+    uint32_t mantissa : 23;
+    uint32_t exp      :  8;
+    uint32_t sign     :  1;
+  }
+  uint32_t val;
+}
+
 FLOAT F_mul_F(FLOAT a, FLOAT b) {
   // assert(0);
   // return 0;
@@ -11,7 +20,24 @@ FLOAT F_mul_F(FLOAT a, FLOAT b) {
 FLOAT F_div_F(FLOAT a, FLOAT b) {
   // assert(0);
   // return 0;
-  
+  assert(b != 0);
+  FLOAT x = Fabs(a);
+  FLOAT y = Fabs(b);
+  FLOAT ret = x / y;
+  x = x % y;
+
+  for (int i = 0; i < 16; i ++) {
+    x <<= 1;
+    ret <<= 1;
+    if (x >= y) {
+      x -= y;
+      ret ++;
+    }
+  }
+  if (((a ^ b) & 0x80000000) == 0x80000000) {
+    ret = -ret;
+  }
+  return ret;
 }
 
 FLOAT f2F(float a) {
@@ -25,13 +51,31 @@ FLOAT f2F(float a) {
    * performing arithmetic operations on it directly?
    */
 
-  assert(0);
-  return 0;
+  // assert(0);
+  // return 0;
+  float_f f;
+  f.val = *((uint32_t*)(void*)&a);
+  int exp = f.exp - 127;
+  FLOAT ret = 0;
+  if (exp ==128) { assert(0); }
+  if (exp >= 0) {
+    int mov = 7 - exp;
+    if (mov >= 0) { ret = (f.mantissa | (1 << 23)) >> mov; }
+    else { ret = (f.mantissa | (1 << 23)) << (-mov); }
+  }
+  else { return 0; }
+  return f.sign == 0 ? ret : -ret;
 }
 
 FLOAT Fabs(FLOAT a) {
-  assert(0);
-  return 0;
+  // assert(0);
+  // return 0;
+  if ((a & 0x80000000) == 0) {
+    return a;
+  }
+  else {
+    return -a;
+  }
 }
 
 /* Functions below are already implemented */
